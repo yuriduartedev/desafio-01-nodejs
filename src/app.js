447251cca9bf9cc1d7a8dae57df084ff945a0f3e
Yuri Duarte <yuriduartedev@gmail.com>
2020-04-09 11:47:43 -0300

    Finaliza desafio

--- src/app.js
+++ src/app.js
@@ -11,13 +11,13 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
   const { title, url, techs } = request.body;
   const repo = {
-    id: uuid,
+    id: uuid(),
     title,
     url,
     techs,
@@ -30,15 +30,46 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: "Repo not found" });
+  }
+
+  const repository = { id, title, url, techs, likes: repositories[repoIndex].likes}
+  repositories[repoIndex] = repository;
+
+  return response.status(200).json(repository);
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: "Repo not found" });
+  }
+
+  repositories.splice(repoIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: "Repo not found" });
+  }
+
+  const repo = repositories[repoIndex]
+
+  repo.likes = repo.likes + 1;
+
+  return response.status(201).json(repo);
 });
 
 module.exports = app;
